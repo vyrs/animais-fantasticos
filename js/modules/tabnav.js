11ce362511b77c992dcaad494605ae95c65dc897
vyrs <vitory.rsilva@hotmail.com>
2020-12-18 22:23:58 -0300

    adicionando melhorias

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,8 +1,12 @@
+import debounce from './debounce.js';
+
 export default class TabNav {
   constructor(menu, content) {
     this.tabMenu = document.querySelectorAll(menu);
     this.tabContent = document.querySelectorAll(content);
     this.activeClass = 'ativo';
+    this.windowQuarto = window.innerHeight * 0.5;
+    this.checkDistance = debounce(this.checkDistance.bind(this), 25);
   }
 
   // Ativa a tab de acordo com o index da mesma
@@ -14,11 +18,29 @@ export default class TabNav {
     this.tabContent[index].classList.add(this.activeClass, direcao);
   }
 
+  getDistance() {
+    this.distance = [...this.tabMenu].map((image) => {
+      const offset = image.getBoundingClientRect().top;
+      return {
+        element: image,
+        offset,
+      };
+    });
+  }
+
+  checkDistance() {
+    this.getDistance();
+    this.distance.forEach((item, index) => {
+      if (item.offset < this.windowQuarto) this.activeTab(index);
+    });
+  }
+
   // Adiciona os eventos nas tabs
   addTabNavEvent() {
     this.tabMenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener('click', () => this.activeTab(index));
     });
+    document.querySelector('.animais-lista').addEventListener('scroll', this.checkDistance);
   }
 
   init() {
